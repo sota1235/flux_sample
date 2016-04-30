ca378c4b429821be01380b67a0b5a3b0518839de
sota1235 <sota1235@gmail.com>
2016-04-30 16:58:42 +0900

    modify: add constans files for flux architecture

---
+++ src/config/actions.js
@@ -0,0 +1,9 @@
+/**
+ * @description Constants for action name.
+ */
+
+/** @type {string} Adding todo action. */
+export const AddTodoAction = 'ADD_TODO';
+
+/** @type {string} Removing todo action. */
+export const RemoveTodoAction = 'REMOVE_TODO';
---
+++ src/config/constanst.js
@@ -0,0 +1,6 @@
+/**
+ * @description Constants for application.
+ */
+
+/** @type {string} Event name of changing store data. */
+export const StoreDataChangeEvent = 'STORE_DATA_CHANGED';
