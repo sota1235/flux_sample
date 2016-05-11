461d30dc1d7a12aa6a5c33751c6d42e3d12ae6b3
sota1235 <sota1235@gmail.com>
2016-05-11 14:07:08 +0900

    add: todo list component

---
+++ src/components/todo-list-component.js
@@ -0,0 +1,29 @@
+/**
+ * @description Todo list component.
+ */
+
+import React from 'react';
+import TodoComponent from './todo-component';
+
+export default class TodoListComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: this.props.todos,
+    };
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.todoso.map(todo =>
+          <TodoComponent
+            id={todo.id}
+            text={todo.text}
+          />
+        )}
+      </ul>
+    );
+  }
+}
